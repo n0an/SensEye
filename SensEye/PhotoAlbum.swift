90cf2cbf7a98c2c04f1e77b5e07819f95be73a5a
n0an <nantog@gmail.com>
2016-12-28 21:53:35 +0300

    iCarousel tested

--- SensEye/PhotoAlbum.swift
+++ SensEye/PhotoAlbum.swift
@@ -14,7 +14,11 @@ class PhotoAlbum {
     var ownerID: String!
     var albumTitle: String!
     var albumDescription: String!
+    
     var albumSize: Int!
+    
+    var albumThumbID: String?
+    
     var albumThumbImageURL: String?
     
     var albumThumbPhoto: Photo?
@@ -42,6 +46,16 @@ class PhotoAlbum {
         if let albumSize = responseObject["size"] as? Int {
             self.albumSize = albumSize
         }
+        
+        if let albumThumbID = responseObject["thumb_id"] as? Int {
+            self.albumThumbID = String(albumThumbID)
+        }
+        
+        if let albumThumbImageURL = responseObject["thumb_src"] as? String {
+            self.albumThumbImageURL = albumThumbImageURL
+        }
+        
+        
         
         if let thumbDict = responseObject["thumb"] as? [String: Any] {
             
