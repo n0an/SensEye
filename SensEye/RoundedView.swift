a44c87850cc8f4c472baf0011d3f37bf8bf8db45
n0an <nantog@gmail.com>
2016-12-30 18:36:06 +0300

    RoundedView added. Album View constructed using two views - innter is RoundedView, ext is PostView

---
+++ SensEye/RoundedView.swift
@@ -0,0 +1,55 @@
+//
+//  RoundedView.swift
+//  SensEye
+//
+//  Created by Anton Novoselov on 30/12/2016.
+//  Copyright © 2016 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+
+
+class RoundedView: UIView{
+    
+    var cornerRadius: CGFloat = 4 {
+        didSet {
+            
+        }
+    }
+    
+    override func layoutSubviews() {
+        self.layer.cornerRadius = cornerRadius
+        self.layer.masksToBounds = cornerRadius > 0
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
