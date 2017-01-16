1a3d746df68873079d0af65e4219864bc6e2ad69
n0an <nantog@gmail.com>
2017-01-16 22:01:06 +0300

    working on Login UI

---
+++ SensEye/WelcomeViewController.swift
@@ -0,0 +1,47 @@
+//
+//  WelcomeViewController.swift
+//  SensEye
+//
+//  Created by Anton Novoselov on 16/01/2017.
+//  Copyright © 2017 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+import Spring
+
+class WelcomeViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBOutlet weak var loginButton: DesignableButton!
+
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+    @IBAction func actionLoginFacebookTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func actionLoginGoogleTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func actionLoginButtonTapped(_ sender: Any) {
+    }
+    
+    
+
+}
