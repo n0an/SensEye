ba5d53242ba4e6ac15339d35d45ebf544005962e
n0an <nantog@gmail.com>
2017-02-05 10:47:43 +0300

    cleanup

--- SensEye/GeneralHelper.swift
+++ SensEye/GeneralHelper.swift
@@ -111,7 +111,7 @@ class GeneralHelper {
     public func showVKAuthorizeActionSheetOnViewController(viewController: UIViewController, completion: @escaping (Bool) -> Void) {
         
         
-        let actionSheet = UIAlertController(title: NSLocalizedString("You have to login to VK to get access to likes and comments feature", comment: "VK Login"), message: NSLocalizedString("You have to login just once", comment: "VK Login"), preferredStyle: .actionSheet)
+        let actionSheet = UIAlertController(title: NSLocalizedString("Authorization to VK required", comment: "VK Login"), message: NSLocalizedString("You have to login to VK to get access to likes and comments features. You have to login just once", comment: "VK Login"), preferredStyle: .actionSheet)
         
         let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
             
