cae05d63ff769a93dd26ed89fe9a741803c28285
n0an <nantog@gmail.com>
2018-04-02 11:06:29 +0300

    pods updated. testFetchPost added

--- SensEyeTests/SensEyeTests.swift
+++ SensEyeTests/SensEyeTests.swift
@@ -10,7 +10,6 @@ import XCTest
 
 @testable import SensEye
 
-
 class SensEyeTests: XCTestCase {
     
     override func setUp() {
@@ -19,6 +18,24 @@ class SensEyeTests: XCTestCase {
     
     override func tearDown() {
         super.tearDown()
+    }
+    
+    func testFetchPost() {
+        
+        let postFetchExpectation = expectation(description: "postFetchExpectation")
+        
+        ServerManager.sharedManager.getFeed(forType: .post, ownerID: groupID, offset: 0, count: 1) { (posts) in
+            
+            if let posts = posts as? [WallPost] {
+                if !posts.isEmpty {
+                    postFetchExpectation.fulfill()
+                }
+            }
+        }
+        
+        waitForExpectations(timeout: 5) { (err) in
+            XCTAssertNil(err, "postFetchTimeout")
+        }
     }
     
 }
